03e64b44419d26e3f6a94d5030e38f56393efdd7
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-08-20 13:15:54 +0200

    EVENTOS

--- firtsAng1/src/app/empleado/empleado.component.ts
+++ firtsAng1/src/app/empleado/empleado.component.ts
@@ -3,10 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
-  styleUrls: ['./empleado.component.css']
+  //styleUrls: ['./empleado.component.css']
+  styles: ["p {background-color: red;}"]
+
 })
+
+  /*
+   * EN EL ESPACION DE LA CLASE ES DONDE GUARDAREMOS CUALQUIER CODIGO REFERENTE A LA CLASE
+   */
 export class EmpleadoComponent implements OnInit {
 
+  nombre = "Juan"; // DECLARACION DE PROPIEDADES PARA USO DE LA INTERPOLACION
+
+  apellido = "Clazadilla";
+
+  edad = 34;
+
+  //empresa = "hola, que tal";
+
+ // llamada(value: String) {} // CREACION DE LA FUNCION DEL EVENTO CARGA DEL VALOR DE LA VARIABLE
 
   constructor() { }
 
--- firtsAng1/src/app/empleados/empleados.component.ts
+++ firtsAng1/src/app/empleados/empleados.component.ts
@@ -10,4 +10,5 @@ import { Component } from "@angular/core";
 
 export class EmpleadosComponent { // CLASE DEL COMPONENTE NUEVO
 
+  nombre = "juan";
 }
